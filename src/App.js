a2eb59bdccfae4c4a6913be670e22f9004f8dd0e
Akash Wadageri <wadagireakash92@gmail.com>
2025-09-13 21:20:42 +0530

    Add new bar and useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Components/Header';
+import {Todos} from "./Components/Todos";
+import Footer  from "./Components/Footer";
+import React, {useState} from 'react';
 
 function App() {
+  const onDelete = (todo) =>{
+    console.log("I am onDelte of todo", todo);
+    // let index = todos.indexOf(todo);
+    // todos.splice(index, 1);
+    setTodos(todos.filter((e) =>{
+      return e!==todo;
+    }))
+  }
+
+  const[todos, setTodos] = useState(
+[
+    {
+      sno: 1,
+      title: "Go to the market",
+      desc: "you need to go to the market to get this job is done1"
+    },
+    {
+      sno: 2,
+      title: "Go to the mall",
+      desc: "you need to go to the market to get this job is done2"
+    },
+    {
+      sno: 3,
+      title: "Go to the ghat",
+      desc: "you need to go to the market to get this job is done3"
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header title="Todos List" searchBar={true}/>
+    <Todos todos = {todos} onDelete={onDelete}/>
+    <Footer/>
+    </>
   );
 }
 
